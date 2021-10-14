6ec6d05aa9e46d4b44bf40361e79872c66ea7851
ABdevs29 <abhishek.elmoro@gmail.com>
2021-10-14 07:13:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+import express from "express";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+async function createConnection () {
+    const MONGO_URL = process.env.MONGO_URL;
+    const client = new MongoClient(MONGO_URL);
+
+    await client.connect();
+    console.log("Mongo Server connected");
+    return client;
+}
+
+createConnection();
+
+app.get('/', (request, response) => {
+    response.send("Camera Equipment Rental API");
+});
+
+app.get('/products', async (request, response) => {
+    const client = await createConnection();
+
+    const products = await client.db("equipment-rental").collection("products").find({}).toArray();
+    response.send(products);
+});
+
+app.post('/products', async (request, response) => {
+    const client = await createConnection();
+    const newProduct = request.body;
+
+    const result = await client.db("equipment-rental").collection("products").insertMany(newProduct);
+    response.send(result);
+});
+
+app.listen(PORT, () => console.log("Server started at Port ", PORT));
